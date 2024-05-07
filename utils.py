4623c52b6399b89069d263be1d96ca4656a0ab38
solyrion <sde101573@gmail.com>
2024-05-07 22:13:42 +0900

    fix: 모델 선택 방식 변경

--- utils.py
+++ utils.py
@@ -4,19 +4,26 @@ import numpy as np
 
 def run_models(image):
     processed_image = preprocess_image(image)
+    results = []
+
     for disease in models:
         model = models[disease]
         result = model.predict(np.array([processed_image])).tolist()[0]
-        # results.append((max(result), disease))
         ind = result.index(max(result))
-        if max(result) >= 0.800:
-            return {"disease_name": disease,
-                    "disease_kr": disease_kr[disease],
-                    "disease_label_kr": disease_label[disease],
-                    "result": result,
-                    "max_possibility_index": ind,
-                    "max_possibility": max(result),
-                    "result_label": disease_label[disease][ind]}
+
+        results.append((max(result), disease, ind, result))
+
+    results.sort(key=lambda x: x[0], reverse=True)
+    final_model = results[0]
+    disease_name = final_model[1]
+
+    return {"disease_name": disease_name,
+            "disease_kr": disease_kr[disease_name],
+            "disease_label_kr": disease_label[disease_name],
+            "result": final_model[-1],
+            "max_possibility_index": final_model[2],
+            "max_possibility": final_model[0],
+            "result_label": disease_label[disease_name][final_model[2]]}
 
 
 def preprocess_image(image):

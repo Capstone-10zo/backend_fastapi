1e22c4138f03b47f63c523d7387a0e5287b5e241
solyrion <sde101573@gmail.com>
2024-06-14 22:48:56 +0900

    fix: 챗봇 모델 수정

--- chatbot/model_load.py
+++ chatbot/model_load.py
@@ -2,11 +2,30 @@ from .qna_bot import load_qna_bot
 
 def load_qna(request):
     # 저장된 파일 로드 -> QnABot 클래스 인스턴스 생성
-    loaded_qna_bot = load_qna_bot('chatbot_model.pkl')
+능    loaded_qna_bot1 = load_qna_bot('chatbot_model.pkl1')
+    loaded_qna_bot2 = load_qna_bot('chatbot_model.pkl2')
+    loaded_qna_bot3 = load_qna_bot('chatbot_model.pkl3')
 
+    if "안녕" in request or "물어봐도" in request or "고마워" in request:
+        qna_bots = [loaded_qna_bot2, loaded_qna_bot3]
+    else:
+        qna_bots = [loaded_qna_bot1]
     # 사용자 입력에 대한 답변 예측
     user_input = request
-    similar_question, predicted_answer, similarity_score = loaded_qna_bot.predict_answer(user_input)
+    result = []
+    for bot in qna_bots:
+        similar_question, predicted_answer, similarity_score = bot.predict_answer(user_input)
+        result.append((similarity_score, similar_question, predicted_answer))
+
+        if similarity_score >= 0.8:
+            break
+
+    result.sort(key = lambda x:x[0], reverse = True)
+
+    similarity_score, similar_question, predicted_answer = result[0]
+    print(similarity_score)
+    print(similar_question)
+    print(predicted_answer)
     return {"입력 질문":user_input, "유사한 질문": similar_question,
             "예측 답변": predicted_answer, "유사도": similarity_score}
 
